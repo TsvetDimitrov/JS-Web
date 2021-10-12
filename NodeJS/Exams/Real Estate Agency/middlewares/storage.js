53ac21419ab86c964852c65635524b61646e6460
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-12 13:05:09 +0300

    homeController

--- NodeJS/Exams/Real Estate Agency/middlewares/storage.js
+++ NodeJS/Exams/Real Estate Agency/middlewares/storage.js
@@ -1,4 +1,10 @@
+const house = require('../services/house');
+
 module.exports = () => (req, res, next) => {
     //TODO import and decorate services
-    req.storage = {};
+    req.storage = {
+        ...house
+    };
+
+    next();
 };
