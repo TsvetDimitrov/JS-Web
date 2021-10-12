12ee0354e52e57f8f047caa7f2cb7449f8f3ffe9
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-12 12:50:36 +0300

    house service

---
+++ NodeJS/Exams/Real Estate Agency/services/house.js
@@ -0,0 +1,41 @@
+const Hotel = require('../../BookingUni/models/Hotel');
+const House = require('../models/House');
+const User = require('../models/User');
+
+async function createHouse() {
+
+}
+
+async function getAllHouses() {
+    const houses = await House.find({}).lean();
+
+    return houses;
+}
+
+async function getHouseById(id) {
+    const house = await House.findById(id).lean();
+
+    return house;
+}
+
+async function editHouse(id, houseData) {
+    const house = await House.findById(id);
+
+    //...//
+
+
+    return house;
+}
+
+async function deleteHouse(id) {
+    return House.findByIdAndDelete(id);
+}
+
+
+module.exports = {
+    createHouse,
+    getAllHouses,
+    getHouseById,
+    editHouse,
+    deleteHouse,
+}
