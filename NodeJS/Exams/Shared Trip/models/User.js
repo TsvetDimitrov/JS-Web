68ea4c01d9cb115bbfabced450bfaeec77590d3c
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-13 14:03:02 +0300

    model as requirements

--- NodeJS/Exams/Shared Trip/models/User.js
+++ NodeJS/Exams/Shared Trip/models/User.js
@@ -1,8 +1,11 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
-    username: {type: String, required: true},
-    hashedPassword: {type: String, required: true}
+    username: { type: String, required: [true, 'Username is required!'] },
+    email: { type: String, required: [true, 'Email is required!'] },
+    gender: { type: String, required: true },
+    hashedPassword: { type: String, required: true },
+    tripHistory: [{ type: Schema.Types.ObjectId, ref: 'Trip', default: [] }]
 });
 
 module.exports = model('User', schema);
