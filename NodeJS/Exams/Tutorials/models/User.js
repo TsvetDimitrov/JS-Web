1bdc654b8d4d5add1e2493c4e14295aa1fe99230
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-15 14:37:35 +0300

    renamed files to .hbs

--- NodeJS/Exams/Tutorials/models/User.js
+++ NodeJS/Exams/Tutorials/models/User.js
@@ -1,8 +1,9 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
-    username: {type: String, required: true},
-    hashedPassword: {type: String, required: true}
+    username: { type: String, required: true },
+    hashedPassword: { type: String, required: true },
+    courses: [{ type: Schema.Types.ObjectId, ref: 'Tutorial', default: [] }]
 });
 
 module.exports = model('User', schema);
