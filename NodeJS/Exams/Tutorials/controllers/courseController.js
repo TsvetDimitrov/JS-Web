f3ea28d9d02b132587e8810c1e59486f57771a02
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-18 00:21:30 +0300

    delete course

--- NodeJS/Exams/Tutorials/controllers/courseController.js
+++ NodeJS/Exams/Tutorials/controllers/courseController.js
@@ -123,4 +123,20 @@ router.post('/edit/:id', isUser(), async (req, res) => {
     }
 });
 
+
+router.get('/delete/:id', isUser(), async (req, res) => {
+    try{
+        const course = await req.storage.getCourseById(req.params.id);
+
+        if(course.owner != req.user._id){
+            throw new Error('Cannot delete course you have\'nt created');
+        }
+
+        await req.storage.deleteCourse(req.params.id);
+        res.redirect('/');
+    }catch(err){
+        console.log(err.message);
+    }
+});
+
 module.exports = router;
