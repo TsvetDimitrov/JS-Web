62e681aeb95c72d97737f326a6aed652690bb3c5
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-28 17:38:28 +0300

    home page loaded

--- NodeJS/Exams/BookingUni-2-Practise/controllers/homeController.js
+++ NodeJS/Exams/BookingUni-2-Practise/controllers/homeController.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const hotels = await req.storage.getAllHotels();
     res.render('home/home', { hotels });
 });
-
-
-
 
 
 module.exports = router;
--- NodeJS/Exams/BookingUni-2-Practise/middlewares/storage.js
+++ NodeJS/Exams/BookingUni-2-Practise/middlewares/storage.js
@@ -1,8 +1,8 @@
-
+const hotelService = require('../services/hotel');
 
 module.exports = () => (req, res, next) => {
     //TODO import and decorate services
-    req.storage = {};
+    req.storage = { ...hotelService };
 
     next();
 };
